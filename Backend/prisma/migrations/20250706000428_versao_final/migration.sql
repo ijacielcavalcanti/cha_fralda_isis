c05fe079dfae2913192dbfd17399a41e94b8b287
Ijaciel Cavalcanti <ijacielcavalcanti21@gmail.com>
2025-07-05 21:05:29 -0300

    versao final do db

--- Backend/prisma/migrations/20250706000428_versao_final/migration.sql
+++ Backend/prisma/migrations/20250706000428_versao_final/migration.sql
@@ -2,6 +2,7 @@
 CREATE TABLE "Convidado" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "nome" TEXT NOT NULL,
+    "celular" TEXT,
     "adultos" INTEGER NOT NULL,
     "criancas" INTEGER NOT NULL DEFAULT 0,
     "confirmacao_presenca" BOOLEAN NOT NULL DEFAULT false,
@@ -30,6 +31,25 @@ CREATE TABLE "ReservaPresente" (
 );
 
 -- CreateTable
+CREATE TABLE "Mimo" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "nome" TEXT NOT NULL,
+    "quantidade_total_desejada" INTEGER NOT NULL,
+    "quantidade_reservada" INTEGER NOT NULL DEFAULT 0,
+    "imagem_url" TEXT
+);
+
+-- CreateTable
+CREATE TABLE "ReservaMimo" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "convidado_id" INTEGER NOT NULL,
+    "mimo_id" INTEGER NOT NULL,
+    "data_reserva" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "ReservaMimo_convidado_id_fkey" FOREIGN KEY ("convidado_id") REFERENCES "Convidado" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "ReservaMimo_mimo_id_fkey" FOREIGN KEY ("mimo_id") REFERENCES "Mimo" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
 CREATE TABLE "MensagemMural" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "nome_remetente" TEXT NOT NULL,
@@ -42,3 +62,6 @@ CREATE TABLE "MensagemMural" (
 
 -- CreateIndex
 CREATE UNIQUE INDEX "ItemPresente_nome_key" ON "ItemPresente"("nome");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Mimo_nome_key" ON "Mimo"("nome");
