9306861dcdc0456f36d156f1a7c52aaea3f9acb4
Ijaciel Cavalcanti <ijacielcavalcanti21@gmail.com>
2025-07-24 19:17:59 -0300

    Atualização do endereço do cha

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,15 +2,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './index.css'; // Manteremos o index.css para o fundo e fontes
-
+import './index.css';
 import { ThemeProvider } from '@mui/material/styles';
-import theme from './Theme.js'; // Importamos nosso tema
+import theme from './theme';
+
+// 1. Importamos o BrowserRouter
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    {/* 2. Envolvemos nossa App com ele */}
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 );
